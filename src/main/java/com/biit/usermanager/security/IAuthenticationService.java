3b971ebad2af311e3f90b1af5d7289441c428553
Jorge Hortelano <jhortelano@biit-solutions.com>
2023-03-27 17:55:21 +0200

    Returning user entity when updating

--- src/main/java/com/biit/usermanager/security/IAuthenticationService.java
+++ src/main/java/com/biit/usermanager/security/IAuthenticationService.java
@@ -9,33 +9,33 @@ import com.biit.usermanager.security.exceptions.UserManagementException;
 
 public interface IAuthenticationService<UserId, GroupId> {
 
-	IUser<UserId> authenticate(String userMail, String password) throws UserManagementException, AuthenticationRequired, InvalidCredentialsException,
-			UserDoesNotExistException;
+    IUser<UserId> authenticate(String userMail, String password) throws UserManagementException, AuthenticationRequired, InvalidCredentialsException,
+            UserDoesNotExistException;
 
-	IGroup<GroupId> getDefaultGroup(IUser<UserId> user) throws UserManagementException;
+    IGroup<GroupId> getDefaultGroup(IUser<UserId> user) throws UserManagementException;
 
-	IUser<UserId> getUserByEmail(String userEmail) throws UserManagementException, UserDoesNotExistException;
+    IUser<UserId> getUserByEmail(String userEmail) throws UserManagementException, UserDoesNotExistException;
 
-	IUser<UserId> getUserById(long userId) throws UserManagementException, UserDoesNotExistException;
+    IUser<UserId> getUserById(long userId) throws UserManagementException, UserDoesNotExistException;
 
-	boolean isInGroup(IGroup<GroupId> group, IUser<UserId> user) throws UserManagementException;
+    boolean isInGroup(IGroup<GroupId> group, IUser<UserId> user) throws UserManagementException;
 
-	IUser<UserId> updatePassword(IUser<UserId> user, String plainTextPassword) throws UserManagementException;
+    IUser<UserId> updatePassword(IUser<UserId> user, String plainTextPassword) throws UserDoesNotExistException, InvalidCredentialsException, UserManagementException;
 
-	IUser<Long> updateUser(IUser<Long> user) throws UserManagementException;
+    IUser<Long> updateUser(IUser<Long> user) throws UserManagementException;
 
-	void reset();
+    void reset();
 
-	IUser<UserId> addUser(IGroup<GroupId> company, String password, String screenName, String emailAddress, String locale, String firstName, String middleName,
-			String lastName) throws UserManagementException;
+    IUser<UserId> addUser(IGroup<GroupId> company, String password, String screenName, String emailAddress, String locale, String firstName, String middleName,
+                          String lastName) throws UserManagementException;
 
-	void deleteUser(IUser<UserId> user) throws UserManagementException;
+    void deleteUser(IUser<UserId> user) throws UserManagementException;
 
-	/**
-	 * If Spring is not available, create the required services. Be carefull, if
-	 * created on this way, each service uses its own pool and is not shared
-	 * with other different beans.
-	 */
-	void createBeans();
+    /**
+     * If Spring is not available, create the required services. Be carefull, if
+     * created on this way, each service uses its own pool and is not shared
+     * with other different beans.
+     */
+    void createBeans();
 
 }
