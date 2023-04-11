2d9da5296594e1f4cee0ddd147c2b170cfa6675f
Jorge Hortelano <jhortelano@biit-solutions.com>
2023-04-11 17:56:28 +0200

    Adding extra method

--- src/main/java/com/biit/usermanager/security/IRoleActivities.java
+++ src/main/java/com/biit/usermanager/security/IRoleActivities.java
@@ -1,15 +1,18 @@
 package com.biit.usermanager.security;
 
-import java.util.Set;
-
 import com.biit.usermanager.entity.IRole;
+
+import java.util.Set;
 
 /**
  * Relationship between a role and the activities that are assigned to this
  * role.
- *
  */
 public interface IRoleActivities {
 
-	Set<IActivity> getRoleActivities(IRole<Long> role);
+    Set<IActivity> getRoleActivities(IRole<Long> role);
+
+    Set<IActivity> getRoleActivities(String roleName);
+
+
 }
