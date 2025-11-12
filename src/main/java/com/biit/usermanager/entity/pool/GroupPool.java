fe9a851c8ecf17f6af70faa9e45cf167253148dd
Jorge Hortelano <jhortelano@biit-solutions.com>
2025-11-12 12:00:11 +0100

    Releasing branch as open source

--- src/main/java/com/biit/usermanager/entity/pool/GroupPool.java
+++ src/main/java/com/biit/usermanager/entity/pool/GroupPool.java
@@ -1,5 +1,26 @@
 package com.biit.usermanager.entity.pool;
 
+/*-
+ * #%L
+ * User Manager Common Utils
+ * %%
+ * Copyright (C) 2015 - 2025 BiiT Sourcing Solutions S.L.
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * #L%
+ */
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -80,9 +101,8 @@ public class GroupPool<UserId, GroupId> extends ElementsByTagPool<GroupId, IGrou
 
 	/**
 	 * Gets all previously stored groups of a user in a site.
-	 * 
-	 * @param siteId
-	 * @param userId
+	 *
+	 * @param groupId
 	 * @return
 	 */
 	public IGroup<GroupId> getGroupById(GroupId groupId) {
